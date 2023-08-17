bd1202a85d92d012f157044d638d071d4b3b2e8c
desombre <alaindesombre@gmail.com>
2023-08-17 16:56:22 +0200

    add contaact & cgu

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+            'title'=>'contact',
+        ]);
+    }
+
+    #[Route('/cgu', name: 'cgu')]
+    public function cgu(): Response
+    {
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+            'title'=>'cgu'
+        ]);
+    }
+}
